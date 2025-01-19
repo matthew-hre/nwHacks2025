5f1f22494b2148efc5ef8ffd569ffb908141e52f
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-01-19 09:50:51 -0700

    moved actions to separate file to fix vercel crash

---
+++ app/addbook/actions.ts
@@ -0,0 +1,106 @@
+import { createClient } from "@/utils/supabase/server";
+
+export async function addIsbnToDb(formData: FormData) {
+  "use server";
+
+  const bookName = formData.get("bookName") as string;
+
+  console.log("bookName", bookName);
+
+  if (!bookName) {
+    return;
+  }
+
+  processIsbn(bookName);
+}
+
+export async function processIsbn(bookName: string) {
+  "use server";
+
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const { data } = await supabase
+    .from("books")
+    .select("book_isbn")
+    .eq("book_name", bookName);
+
+  if (data && data.length > 0) {
+    // the book already exists in our DB, so we can just add the user's ID and the book's ISBN to the user_id_isbn table
+    const isbn = data[0].book_isbn;
+
+    const { error } = await supabase.from("user_id_isbn").insert([
+      {
+        id: user?.id,
+        isbn: isbn,
+      },
+    ]);
+
+    if (error) {
+      console.error("Error adding book to user's shelf:", error.message);
+      return;
+    }
+
+    return;
+  }
+
+  // if the book doesn't exist in our DB, we need to add it to the books table first
+  const apiKey = process.env.ISBN_API_KEY; // Store your API key in environment variables for security
+  const apiUrl = `https://api2.isbndb.com/books/${encodeURIComponent(
+    bookName
+  )}`;
+
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+  };
+
+  if (apiKey) {
+    headers["Authorization"] = apiKey;
+  }
+
+  const response = await fetch(apiUrl, {
+    headers,
+  });
+
+  const bookData = await response.json();
+
+  if (bookData.error) {
+    console.error("Error fetching book data:", bookData.error);
+    return;
+  }
+
+  const book = bookData.books.find(
+    (book: { title: string; isbn13: string; authors: string[] }) =>
+      book.title === bookName
+  );
+  const isbn = book.isbn13;
+
+  const author = book.authors[0];
+
+  const { error } = await supabase.from("books").insert([
+    {
+      book_isbn: isbn,
+      book_name: bookName,
+      book_author: author,
+    },
+  ]);
+
+  if (error) {
+    console.error("Error adding book to database:", error.message);
+    return;
+  }
+
+  const { error: error2 } = await supabase.from("user_id_isbn").insert([
+    {
+      id: user?.id,
+      isbn: isbn,
+    },
+  ]);
+
+  if (error2) {
+    console.error("Error adding book to user's shelf:", error2.message);
+    return;
+  }
+}
